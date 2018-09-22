7b73cb120566706288e8ea5f39b1b1ba64cb50dd
ning <ningyuan.sg@gmail.com>
2018-09-23 02:02:53 +0800

    Add docstrings to module validate

--- phonebook/validate.py
+++ phonebook/validate.py
@@ -3,12 +3,15 @@ import re, urllib.parse
 import db
 
 def is_code_valid(code: str) -> bool:
+    '''Returns if the given code is a valid NUS module code'''
     return re.match('^[A-Z]{2,3}\d{4}[A-Z]?$', code) is not None
 
 def is_code_unique(code: str) -> bool:
+    '''Returns if the given code will be unique when added to the db'''
     return db.get_group(code) is None
 
 def is_url_valid(url: str) -> bool:
+    '''Returns if the given url is a valid telegram group invite link.'''
     fragments = urllib.parse.urlparse(url)
     return (
         fragments.scheme == 'https'
@@ -20,4 +23,5 @@ def is_url_valid(url: str) -> bool:
     )
 
 def is_url_unique(url: str) -> bool:
+    '''Returns if the given url will be unique when added to the db'''
     return db.get_group_by_url(url) is None
